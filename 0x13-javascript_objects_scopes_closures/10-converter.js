9bb5ef050c73dfc804592c3fdcc1437123ee0839
fawoyera <femiawoyera@gmail.com>
2024-04-14 20:00:59 -0700

    update number base conversion using toString()

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,16 +1,7 @@
 #!/usr/bin/node
 exports.converter = function (base) {
-  function numBase(number) {
-    const result = [];
-    do {
-      result.push(number % base);
-      number = Math.floor(number / base);
-    }
-    while (number > 0);
-    for (let i = result.length - 1; i >= 0; i--) {
-      process.stdout.write(result[i].toString());
-    }
-    return '';
+  function numBase (number) {
+    return number.toString(base);
   }
   return numBase;
 };
