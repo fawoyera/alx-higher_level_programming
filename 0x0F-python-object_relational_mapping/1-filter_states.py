dae71400acf8efad8c2e7b69345e8ebaa5d6c044
fawoyera <femiawoyera@gmail.com>
2024-04-19 11:09:25 +0000

    list all states with a name starting with N from the database hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+    Module Script to list all states from the database hbtn_0e_0_usa
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    connection = MySQLdb.connect(host="localhost", port=3306, user=username,
+                                 passwd=password, db=database, charset="utf8")
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT * FROM states WHERE states.name\
+                   LIKE 'N%' ORDER BY states.id")
+    query_results = cursor.fetchall()
+
+    for row in query_results:
+        print(row)
+
+    cursor.close()
+    connection.close()
