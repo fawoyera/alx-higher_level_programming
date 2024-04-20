a734abda59f6d80097522695225cfeb3a511f7d2
fawoyera <femiawoyera@gmail.com>
2024-04-20 09:20:23 +0000

    print the State object with the name passed as argument safe from sql injection

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+    Module script to list all State objects that contain the letter a
+    from the database hbtn_0e_6_usa
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+name = sys.argv[4]
+if (__name__ == "__main__") and (';' not in name) and ('\'' not in name):
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+                           sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    query_state = session.query(State).filter(State.name.like(name)
+                                              ).order_by(State.id).first()
+
+    if query_state is None:
+        print("Not found")
+    else:
+        print(query_state.id)
+
+    session.close()
