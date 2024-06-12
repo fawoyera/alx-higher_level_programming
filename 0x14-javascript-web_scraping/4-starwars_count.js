ba3009d1c7e2f303fc8503164971c24ee4f70afd
fawoyera <femiawoyera@gmail.com>
2024-06-12 14:40:41 +0000

    add script to print the no of movies with character Wedge Antilles update

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,23 +1,38 @@
 #!/usr/bin/node
 /* Script to print the number of movies where the character "Wedge Antilles" is present */
 
-const id = 18;
-const wedgeAntillesURL = `https://swapi-api.alx-tools.com/api/people/${id}/`;
+const request = require('request-promise-native');
 const URL = `${process.argv[2]}`;
 
-const request = require('request');
-
-request(URL, function (error, response, body) {
-  if (error) {
-    console.error('error:', error);
-  } else {
+async function getCount () {
+  try {
+    const body = await request(URL);
     const data = JSON.parse(body);
     let count = 0;
+
     for (const datum of data.results) {
-      if (datum.characters.includes(wedgeAntillesURL)) {
-        count++;
+      let found = false;
+
+      for (const characterURL of datum.characters) {
+        const characterBody = await request(characterURL);
+        const character = JSON.parse(characterBody);
+
+        if (character.name === 'Wedge Antilles') {
+          count++;
+          found = true;
+          break; // exit the inner loop
+        }
+      }
+
+      if (found) {
+        continue; // skip to the next datum if Wedge Antilles is found
       }
     }
+
     console.log(count);
+  } catch (error) {
+    console.error('error:', error);
   }
-});
+}
+
+getCount();
