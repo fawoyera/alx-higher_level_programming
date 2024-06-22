4154c4ec440ff7afb5b062509b3b38cfded0b7f7
fawoyera <femiawoyera@gmail.com>
2024-06-22 12:25:42 +0000

    add script to fetch and display the value of hello from URL update

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
 /* script to fetch and display the value of "hello" from a URL that returns a json */
 
-$.get('https://hellosalut.stefanbohacek.dev/?lang=fr', function (response) {
-  $('div#hello').text(response.hello);
+document.addEventListener('DOMContentLoaded', () => {
+  $.get('https://hellosalut.stefanbohacek.dev/?lang=fr', function (response) {
+    $('div#hello').text(response.hello);
+  });
 });
